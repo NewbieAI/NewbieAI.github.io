08847c5659469ef445979f242f717e9f13d39c42
NewbieAI <admin@tlsiejkjwiiiils.dx.am>
2020-05-12 14:50:45 -0400

    implemented core minesweeper algorithm and view

--- scripts/minesweeper.js
+++ scripts/minesweeper.js
@@ -13,26 +13,52 @@ class Minesweeper {
         // canvas element
 
         this.canvas = document.createElement("canvas");
+        this.canvas.setAttribute("id", "minesweeper");
         this.canvas.setAttribute("class", "screen");
         this.canvas.setAttribute("width", "1600");
         this.canvas.setAttribute("height", "800");
+        this.canvas.addEventListener(
+            "click",
+            this.clickHandler.bind(this),
+        );
+        this.canvas.addEventListener(
+            "contextmenu",
+            (e) => {
+                //e.stopPropagation();
+                e.preventDefault();
+                return false;
+            }
+        );
+
         this.screen = this.canvas.getContext("2d");
         document.body.append(this.canvas);
 	
         this.mines = Minesweeper.INITIAL_MINES;
         this.state = [];
-        for (let i = 0; i < Minesweeper.ROW; i++) {
-            this.state[i] = [];
-            for (let j = 0; j < Minesweeper.COL; j++) {
-                this.state[i][j] = 0;
-                this.render(i, j);
-            }
-        }
+        this.flags = new Set();
+        this.qMarks = new Set();
+        
         this.reset();
 
     }
 
+    destroy() {
+        this.canvas.remove();
+    }
+
     reset() {
+        this.flags.clear();
+        this.qMarks.clear();
+
+        for (let i = 0; i < Minesweeper.ROW; i++) {
+            this.state[i] = [];
+            for (let j = 0; j < Minesweeper.COL; j++) {
+                this.state[i][j] = 0;
+                this.renderCell(i, j);
+            }
+        }
+
+        // insert mines into the grid
         let spaces = [];
         let totalSize = Minesweeper.COL * Minesweeper.ROW;
         for (let k = 0; k < totalSize; k++) {
@@ -79,24 +105,28 @@ class Minesweeper {
     reveal(i, j) {
         // reveals the cell at (i, j)
         this.state[i][j] -= 10;
+        this.renderCell(i, j);
         if (this.state[i][j] != -10) {
             return this.state[i][j] < -10 ? true : false;
         }
 
-        queue = [i, j];
+        let queue = [i, j];
         let k = 0;
         let a, b;
         while (queue.length > k) {
             a = queue[k];
             b = queue[k + 1];
             k += 2;
-            if (this.data[i][j] == -10) {
+            if (this.state[a][b] == -10) {
                 this.runAround(
                     a, 
                     b,
                     (x, y) => {
-                        this.state[x][y] -= 10;
-                        queue.push(x, y);
+                        if (this.state[x][y] >= 0) {
+                            this.state[x][y] -= 10;
+                            this.renderCell(x, y);
+                            queue.push(x, y);
+                        }
                     }
                 );
             }
@@ -104,13 +134,17 @@ class Minesweeper {
         return false;
     }
 
-    render(i, j) {
+    mark(i, j) {
+        this.renderCell(i, j);
+    }
+
+    renderCell(i, j) {
         if (this.state[i][j] >= -1) {
             this.screen.fillStyle = (
                 "rgba(" + 
                 String(0 + ((Math.random() * 20) | 0)) + ", " +
-                String(125 + ((Math.random() * 25) | 0)) + ", " +
-                String(200 + ((Math.random() * 50) | 0)) + ", " +
+                String(125 + ((Math.random() * 20) | 0)) + ", " +
+                String(200 + ((Math.random() * 20) | 0)) + ", " +
                 String(0.5 + (Math.random() * 0.2)) +
                 ")"
             );
@@ -122,11 +156,69 @@ class Minesweeper {
             );
             return;
         }
-        if (this.state[i][j] < -10) {
-            // mines, flags, or question mark
+        if (this.state[i][j] == -11) {
+            this.renderMine(i, j);
         } else {
-            // numbers
+            this.renderDigit(i, j);
         }
     }
+
+    renderDigit(i, j) {
+        let color = [
+            "rgba(55, 220, 110, 0.5)",
+            "rgb(0, 0, 110)",
+            "rgb(0, 110, 0)",
+            "rgb(165, 0, 0)",
+            "rgb(110, 0, 110)",
+            "rgb(55, 55, 0)",
+            "rgb(220, 110, 0)",
+            "rgb(0, 55, 110)",
+            "rgb(55, 110, 220)",
+            "rgb(55, 220, 110)",
+        ];
+
+        this.screen.fillStyle = color[0];
+        this.screen.fillRect(
+            j * 32,
+            i * 32,
+            32,
+            32,
+        );
+
+        let n = this.state[i][j] + 10;
+        if (n > 0) {
+            this.screen.fillStyle = color[n];
+            this.screen.font = "32px bold serif";
+            this.screen.textAlign = "center";
+            this.screen.textBaseline = "middle";
+            this.screen.fillText(
+                String(n),
+                j * 32 + 16,
+                i * 32 + 16,
+            );
+        }
+    }
+
+    renderMine(i, j) {
+        alert("rendering mine");
+    };
+
+    renderFlag(i, j) {
+        alert("rendering flag");
+    }
+
+    renderQuestionMark(i, j) {
+        alert("rendering question mark");
+    }
     
+    clickHandler(e) {
+        e.preventDefault();
+        let rect = this.canvas.getBoundingClientRect();
+        let j = 0 | (e.clientX - rect.x) / 16;
+        let i = 0 | (e.clientY - rect.y) / 16;
+
+        if (this.state[i][j] >= -1) {
+            let isGameEnded = this.reveal(i, j);
+        }
+    }
 }
